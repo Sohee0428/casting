8029bc43f5a47d89b08dfb92b43c0c48a18ba4b6
Sohee <sohee3480@gmail.com>
2021-05-07 19:51:59 +0900

    for문  - 2, 4, 6, 8, 10 출력하기

--- app/src/main/java/com/example/casting/MainActivity.kt
+++ app/src/main/java/com/example/casting/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.casting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for( i in 1..5) {
+
+            val a = i*2
+            Log.d("숫자", a.toString() )
+        }
+
+        for ( i in 2..10 step 2) {
+            Log.d("숫자", i.toString())
+        }
     }
 
     val num1 = "2000"
@@ -14,4 +27,6 @@ class MainActivity : AppCompatActivity() {
     val koreanAge = 2021 - num1.toInt() + 1
 
 
+
 }
+
